7dae4fc32f02fa627cbc72bf5aa98a91af0db32d
Jay Clark <jay.clark.mail@gmail.com>
2023-03-19 08:33:28 +0000

    Base code

---
+++ src/PipelineExample.App1.Tests/UnitTest1.cs
@@ -0,0 +1,29 @@
+namespace PipelineExample.App1.Tests;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void TestMethod2()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void TestMethod3()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void TestMethod4()
+    {
+        Assert.IsTrue(true);
+    }
+}
---
+++ src/PipelineExample.App3.Tests/UnitTest1.cs
@@ -0,0 +1,23 @@
+namespace PipelineExample.App3.Tests;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void TestMethod2()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void TestMethod3()
+    {
+        Assert.IsTrue(true);
+    }
+}
